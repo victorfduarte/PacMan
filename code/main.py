3e8636af95b068f4e2e20cba5f68a5c72c795546
Paulo Victor <victorfduarte18@gmail.com>
2022-06-29 13:27:02 -0300

    tela start finalizada

--- code/main.py
+++ code/main.py
@@ -13,6 +13,8 @@ class Game:
         pygame.display.set_caption(constantes.TITULO_JOGO)
         self.relogio = pygame.time.Clock()
         self.isrunning = True
+        self.fonte = pygame.font.match_font(constantes.FONTE)
+        self.carregar_arquivos()
     
 
     def novo_jogo(self):
@@ -41,7 +43,6 @@ class Game:
                 self.isrunning = False
 
 
-
     def atualizar_sprites(self):
         # Atualizar Sprites
         self.todas_as_sprites.update()
@@ -57,13 +58,59 @@ class Game:
     def carregar_arquivos(self):
         # Carregar os arquivos do jogo
         diretorio_imagens = os.path.join(os.getcwd(), 'imagens')
-
+        self.diretorio_audios = os.path.join(os.getcwd(), 'audios')
+        self.spritesheet = os.path.join(diretorio_imagens, constantes.SPRITESHEET)
+        self.pacman_start_logo = os.path.join(diretorio_imagens, constantes.PACMAN_START_LOGO)
+        
+        self.pacman_start_logo = pygame.image.load(self.pacman_start_logo).convert()
     
 
-    def mostrar_tela_start(self):...
+    def mostrar_texto(self, texto, tamanho, cor, x, y):
+        # Exibe um texto na tela do jogo
+        fonte = pygame.font.Font(self.fonte, tamanho)
+        mensagem = fonte.render(texto, True, cor)
+        mensagem_rect = mensagem.get_rect()
+        mensagem_rect.midtop = (x, y)
+        self.tela.blit(mensagem, mensagem_rect)
+    
+
+    def mostrar_start_logo(self, x, y):
+        start_logo_rect = self.pacman_start_logo.get_rect()
+        start_logo_rect.midtop = (x, y)
+        self.tela.blit(self.pacman_start_logo, start_logo_rect)
 
 
-    def mostrar_tela_game_over(self):...
+    def mostrar_tela_start(self):
+        pygame.mixer.music.load(os.path.join(self.diretorio_audios, constantes.MUSICA_START))
+        pygame.mixer.music.play()
+
+        self.mostrar_start_logo(constantes.LARGURA//2, 20)
+        self.mostrar_texto('-Pressione uma tecla para jogar', 32, constantes.YELLOW,
+                            constantes.LARGURA//2, 320)
+        self.mostrar_texto('Desenvolvido por Victor Duarte', 19, constantes.WHITE,
+                            constantes.LARGURA//2, 570)
+        pygame.display.flip()
+        self.esperar_por_jogador()
+    
+
+    def esperar_por_jogador(self):
+        esperando = True
+        while esperando:
+            self.relogio.tick(constantes.FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    esperando = False
+                    self.isrunning = False
+                
+                if event.type == pygame.KEYDOWN:
+                    esperando = False
+                    pygame.mixer.music.stop()
+                    pygame.mixer.Sound(os.path.join(self.diretorio_audios, constantes.TECLA_START)).play()
+
+
+
+    def mostrar_tela_game_over(self): ...
+
 
 
 g = Game()
